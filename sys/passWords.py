4f24b6b46b229a48b47ad7a4d5775de886eb42f0
SunYunhang <suiyunshuo@gmail.com>
2022-04-29 14:45:24 +0800

    Copy password to clipboard

---
+++ sys/passWords.py
@@ -0,0 +1,20 @@
+# pw.py - An insecure password locker program
+
+import pyperclip
+import sys
+
+PASSWORDS  = {'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
+             'blog': 'VmALvQyKAxiVH5G8v01if1MLZF3sdt',
+             'luggage': '12345'}
+             
+if len(sys.argv) < 2:
+    print('Usage: python passWords.py [account] - copy account password')
+    sys.exit()
+
+account = sys.argv[1]
+
+if account in PASSWORDS:
+    pyperclip.copy(PASSWORDS[account])
+    print('Password for ' + account + ' copied to clipboard.')
+else:
+    print('There is no account named ' + account)
